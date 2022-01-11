f31da47eaa72e7888846d9d9024f3e3fcc468e71
Racheldehaan <46484261+Racheldehaan@users.noreply.github.com>
2022-01-11 15:10:01 +0100

    Line class, functies main en read csv
    Line class main en 2de read csv aangemaakt

--- main.py
+++ main.py
@@ -6,20 +6,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # read csv  print_0 and create chips
-def read_csv(filename):
+def read_csv_chips(filename):
     with open(filename) as file:
         csvreader = csv.reader(file)
         next(csvreader)
         chip_list = []
-
+        
+        # creates chips with id, x coordinate and y coordinate
         for row in csvreader:
-            # creates chip with id, x coordinate and y coordinate
             chip = Chip(row[0], row[1], row[2])
 
             chip_list.append(chip)
-        
-        create_grid(chip_list)
+    return chip_list
 
+def read_csv_netlist(filename):
+    with open(filename) as file:
+        csvreader = csv.reader(file)
+        next(csvreader)
+        netlist = []
+
+        for number, row in enumerate(csvreader):
+            line = Line(number, row[0], row[1],[])
+
+            netlist.append(line)
+
+    return netlist
+
+
+
+# def find_routes(chip_list):
+#     find_route(x_start, y_start x_end, y_end)
+#     pass
+
+# def find_route(x_start, y_start x_end, y_end):
+#     pass
 
 # visualize grid
 def create_grid(chip_list):
@@ -37,12 +57,12 @@ def create_grid(chip_list):
     for i, txt in enumerate(id_list):
         plt.annotate(txt, (x_list[i], y_list[i]), ha='center', va='center')
 
-    plt.xlim([min(x_list) - 1, max(x_list) + 1])
-    plt.ylim([min(y_list) - 1, max(y_list) + 1])
+    plt.xlim([min(x_list) - 1, max(x_list) + 2])
+    plt.ylim([min(y_list) - 1, max(y_list) + 2])
     plt.grid(visible=True, zorder=0)
     plt.title('Circuit Board Grid')
     plt.tight_layout()
-    plt.savefig("plots/plot1.png")
+    plt.savefig("gates&netlists/plots/plot1.png")
 
 # chip class that has a function that
 class Chip:
@@ -51,4 +71,17 @@ class Chip:
         self.x = int(x)
         self.y = int(y)
 
-read_csv("gates&netlists/chip_0/print_0.csv")
+class Line:
+    def __init__(self, id, chip_start, chip_end, route):
+        self.id = id
+        self.start = chip_start
+        self.end = chip_end
+        self.route = route
+
+def main():
+    chip_list = read_csv_chips("gates&netlists/chip_0/print_0.csv")
+    netlist = read_csv_netlist("gates&netlists/chip_0/netlist_1.csv")
+    # routes = find_routes()
+    create_grid(chip_list)
+
+main()
