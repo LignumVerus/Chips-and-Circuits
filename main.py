22801dbc128f6982ffa2f4f4bcfd7f1bcdfe2feb
Racheldehaan <46484261+Racheldehaan@users.noreply.github.com>
2022-01-19 14:21:37 +0100

    black gerund

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-'''
+"""
 * main.py
 * Finds best routes between chips
 * 
 * Viola Koers 12213101
 * Finn Peranovic 12740454
 * Rachel de Haan 12423254
-'''
+"""
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,21 +14,21 @@ import random
 
 
 def read_csv_chips(filename, board):
-    '''
+    """
     reads csv  print_0 and create chips
-    '''
+    """
 
     with open(filename) as file:
         csvreader = csv.reader(file)
         next(csvreader)
-        
+
         chips_dict = {}
 
         # creates chips with id, x coordinate and y coordinate
         for row in csvreader:
             # Chip(id, x, y), DOET NU NIKS!!
             chip = Chip(row[0], row[1], row[2])
-            #chip_list.append(chip)
+            # chip_list.append(chip)
 
             # add x and y coordinate to board
             board.add_chip(chip.x, chip.y)
@@ -40,9 +40,9 @@ def read_csv_chips(filename, board):
 
 
 def read_csv_netlist(filename):
-    '''
+    """
     reads cvc file with netlist
-    '''
+    """
     with open(filename) as file:
         csvreader = csv.reader(file)
         next(csvreader)
@@ -60,9 +60,9 @@ def read_csv_netlist(filename):
 
 
 def find_routes(chips_dict, netlist):
-    '''
+    """
     Needs to return list of Line
-    '''
+    """
     # gets board size
     min_x, max_x, min_y, max_y = get_board_size(chips_dict)
 
@@ -70,9 +70,11 @@ def find_routes(chips_dict, netlist):
     for i, line in enumerate(netlist):
         start_coordinate = chips_dict[line.start]
         end_coordinate = chips_dict[line.end]
-        line.route = find_random_route(start_coordinate, end_coordinate, chips_dict, min_x, max_x, min_y, max_y)   
+        line.route = find_random_route(
+            start_coordinate, end_coordinate, chips_dict, min_x, max_x, min_y, max_y
+        )
 
-    #board.add_lines(route1, route2)
+    # board.add_lines(route1, route2)
     return netlist
 
     # find_route(x_start, y_start x_end, y_end)
@@ -84,11 +86,12 @@ def find_routes(chips_dict, netlist):
 #     pass
 
 
-
-def find_random_route(start_coordinate, end_coordinate, chips_dict, min_x, max_x, min_y, max_y):
-    '''
+def find_random_route(
+    start_coordinate, end_coordinate, chips_dict, min_x, max_x, min_y, max_y
+):
+    """
     creates random route
-    '''
+    """
     route = []
     route.append(start_coordinate)
 
@@ -97,43 +100,62 @@ def find_random_route(start_coordinate, end_coordinate, chips_dict, min_x, max_x
     # gets list of chips not on start and end coordinate
     invalid_chip_coords = list(chips_dict.values())
     invalid_chip_coords.remove(end_coordinate)
-    
+
     while current_coordinate != end_coordinate:
         # get all valid directions
-        choices = valid_directions(current_coordinate, invalid_chip_coords, route, min_x, max_x, min_y, max_y)
+        choices = valid_directions(
+            current_coordinate, invalid_chip_coords, route, min_x, max_x, min_y, max_y
+        )
 
         # update current coord with random choice
         current_coordinate = random.choice(choices)
 
         # add coord to route
         route.append(current_coordinate)
-        
+
     return route
 
 
-def valid_directions(current_coordinate, invalid_chip_coords, route, min_x, max_x, min_y, max_y):
-    '''
+def valid_directions(
+    current_coordinate, invalid_chip_coords, route, min_x, max_x, min_y, max_y
+):
+    """
     Finds all possible directions from point in grid
-    '''
+    """
     choices = []
 
     # left
-    if current_coordinate[0] - 1 >= min_x and (current_coordinate[0] - 1, current_coordinate[1]) not in invalid_chip_coords:
+    if (
+        current_coordinate[0] - 1 >= min_x
+        and (current_coordinate[0] - 1, current_coordinate[1])
+        not in invalid_chip_coords
+    ):
         choices.append((current_coordinate[0] - 1, current_coordinate[1]))
-    
+
     # right
-    if current_coordinate[0] + 1 <= max_x and (current_coordinate[0] + 1, current_coordinate[1]) not in invalid_chip_coords:
+    if (
+        current_coordinate[0] + 1 <= max_x
+        and (current_coordinate[0] + 1, current_coordinate[1])
+        not in invalid_chip_coords
+    ):
         choices.append((current_coordinate[0] + 1, current_coordinate[1]))
 
     # down
-    if current_coordinate[1] - 1 >= min_y and (current_coordinate[0], current_coordinate[1] - 1) not in invalid_chip_coords:
+    if (
+        current_coordinate[1] - 1 >= min_y
+        and (current_coordinate[0], current_coordinate[1] - 1)
+        not in invalid_chip_coords
+    ):
         choices.append((current_coordinate[0], current_coordinate[1] - 1))
-    
+
     # up
-    if current_coordinate[1] + 1 <= max_y and (current_coordinate[0], current_coordinate[1] + 1) not in invalid_chip_coords:
+    if (
+        current_coordinate[1] + 1 <= max_y
+        and (current_coordinate[0], current_coordinate[1] + 1)
+        not in invalid_chip_coords
+    ):
         choices.append((current_coordinate[0], current_coordinate[1] + 1))
 
-    
     # check if coord is backtracking
     if len(route) > 2:
         # cannot go back to previous coordinate
@@ -146,9 +168,9 @@ def valid_directions(current_coordinate, invalid_chip_coords, route, min_x, max_
 
 
 def get_board_size(chips_dict):
-    '''
+    """
     gets max and min coordinates
-    '''
+    """
     x_list = []
     y_list = []
 
@@ -156,7 +178,7 @@ def get_board_size(chips_dict):
         x_list.append(coord[0])
         y_list.append(coord[1])
 
-    min_x = min(x_list) - 1 
+    min_x = min(x_list) - 1
     max_x = max(x_list) + 1
     min_y = min(y_list) - 1
     max_y = max(y_list) + 1
@@ -164,18 +186,16 @@ def get_board_size(chips_dict):
     return min_x, max_x, min_y, max_y
 
 
-
 def create_grid(chips_dict, netlist_routes):
-    '''
+    """
     visualizes grid
-    '''
+    """
     id_list = chips_dict.keys()
     x_list = []
     y_list = []
     for coordinate in chips_dict.values():
         x_list.append(coordinate[0])
         y_list.append(coordinate[1])
-
 
     x_lines = []
     y_lines = []
@@ -199,9 +219,9 @@ def create_grid(chips_dict, netlist_routes):
 
 
 def costs(netlist):
-    '''
+    """
     computes costs
-    '''
+    """
     n = 0
     route_coords = []
     for line in netlist:
@@ -223,9 +243,9 @@ def costs(netlist):
 
 
 def create_output(netlist_routes, chip, net):
-    '''
+    """
     writes output to CSV file
-    '''
+    """
     with open(f"gates&netlists/chip_{chip}/output.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["net", "wires"])
@@ -258,24 +278,24 @@ class Board:
     def __init__(self):
         # {(1,1): (chip_id, [line_id1, line_id2]}
         self.board = {}
-    
+
     # add chip to dictionary with coordinates as key
     def add_chip(self, chip_x, chip_y):
-        #TODO: add chip to dictonary
-        #if coordinate not in dict, add to dict and add chip
-        #else update entry
+        # TODO: add chip to dictonary
+        # if coordinate not in dict, add to dict and add chip
+        # else update entry
         pass
 
     # add line to dictionary with coordinates as key
     def add_line(self, line):
-        #TODO: add line to dictionary
-        #if coordinate not in dict, add to dict and add line
-        #else update entry
+        # TODO: add line to dictionary
+        # if coordinate not in dict, add to dict and add line
+        # else update entry
         pass
 
 
 def main(chip, net):
-    # create a board 
+    # create a board
     board = Board()
     chips_dict = read_csv_chips(f"gates&netlists/chip_{chip}/print_{chip}.csv", board)
     netlist = read_csv_netlist(f"gates&netlists/chip_{chip}/netlist_{net}.csv")
@@ -284,5 +304,6 @@ def main(chip, net):
 
     create_output(netlist_routes, chip, net)
 
-# arg1 = chip , arg2 = net 
+
+# arg1 = chip , arg2 = net
 main(sys.argv[1], sys.argv[2])
